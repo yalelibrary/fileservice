91c421989ed02ec89a5bad209534aa8abe87e901
Osman Din <osman.din.gatech@gmail.com>
2016-05-26 20:05:12 -0400

    Update Indexer endpoint

--- src/main/java/edu/yale/library/fileservice/Indexer.java
+++ src/main/java/edu/yale/library/fileservice/Indexer.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -12,22 +11,21 @@ import static org.slf4j.LoggerFactory.getLogger;
 /**
  * Kicks off indexing. Meant to replace with a cron type process
  */
-@Path("/index")
+@Path("/indexer")
 public class Indexer {
 
     private final Logger logger = getLogger(this.getClass());
 
     @GET
-    @Path("/{param}")
-    public Response get(@PathParam("param") String msg) {
-        logger.debug("INDEX request for:{}", msg);
-        DBManager dbManager = new DBManager();
+    @Path("/index")
+    public Response get() {
+        final DBManager dbManager = new DBManager();
         try {
             dbManager.insert();
             return Response.status(200).build();
         } catch (Exception e) {
             logger.error("Error:", e);
         }
-        return Response.status(404).build(); //TODO
+        return Response.status(404).build();
     }
 }
